6c04206ece7580b8f0a52706d8191bc37d087431
César Blanco <cesarfbg10@gmail.com>
2020-03-20 11:27:37 -0500

    Ajustado cálculo de TRM promedio

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -6,51 +6,33 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent {
 
   trmPromedio: string;
   pulseFlag = false;
 
   constructor( public http: HttpClient ) {
-    this.http.get('https://www.datos.gov.co/api/id/32sa-8pi3.json?$query=select%20*%20order%20by%20`unidad`%20asc%20limit%2020')
+    this.http.get('https://www.datos.gov.co/api/id/32sa-8pi3.json?$query=select%20*%20order%20by%20`unidad`%20asc%20limit%2040')
       .subscribe((tasas: {valor: string, unidad: string, vigenciadesde: string, vigenciahasta: string }[]) => {
         this.trmPromedio = this.calcularPromedioTRM(tasas);
       });
-    const pulsetimer = setInterval(() => {
+    setInterval(() => {
       this.pulseFlag = !this.pulseFlag;
     }, 600);
   }
 
   calcularPromedioTRM(tasas: {valor: string, unidad: string, vigenciadesde: string, vigenciahasta: string }[]) {
-    const tasasArr: number[] = [];
-    const fechaLimite = new Date(tasas[0].vigenciadesde);
-    fechaLimite.setDate(fechaLimite.getDate() - 20);
-    for (const tasa of tasas) {
-      const fechaDesdeTasa = new Date(tasa.vigenciadesde);
-      if (fechaDesdeTasa.getTime() > fechaLimite.getTime()) {
-        if (tasa.vigenciadesde !== tasa.vigenciahasta) {
-          const fechaDesde = new Date(tasa.vigenciadesde).getTime();
-          const fechaHasta = new Date(tasa.vigenciahasta).getTime();
-          const diferencia = ((fechaHasta - fechaDesde) / 1000 / 60 / 60 / 24) + 1; // Días
-          for (let i = 0; i < diferencia; i++) {
-            tasasArr.push(Number(tasa.valor));
-          }
-        } else {
-          tasasArr.push(Number(tasa.valor));
-        }
-      } else {
-        break;
-      }
-    }
-    return ( this.sumarArray(tasasArr) / tasasArr.length ).toFixed(0);
-  }
-
-  sumarArray(tasas: number[]): number {
-    let suma = 0;
+    const mesActual = new Date(tasas[0].vigenciadesde).getMonth();
+    let sumaTasas = 0;
+    let contador = 0;
     tasas.forEach((tasa) => {
-      suma += tasa;
+      if (new Date(tasa.vigenciahasta).getMonth() === mesActual) {
+        sumaTasas += Number(tasa.valor);
+        contador++;
+      }
     });
-    return suma;
+    return (sumaTasas / contador).toFixed(0);
   }
 
 }
