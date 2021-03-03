f7bbce142c0b4cc7c5813f51fd102cc165c2c6cb
Soporte IT <soporteit@CALIER.local>
2021-03-03 09:48:03 -0500

    Comentada Consulta a API TRM

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -9,17 +9,17 @@ import { HttpClient } from '@angular/common/http';
 
 export class NavbarComponent {
 
-  trmPromedio: string;
-  pulseFlag = false;
+  // trmPromedio: string;
+  // pulseFlag = false;
 
   constructor( public http: HttpClient ) {
-    this.http.get('https://www.datos.gov.co/api/id/32sa-8pi3.json?$query=select%20*%20order%20by%20`unidad`%20asc%20limit%2040')
-      .subscribe((tasas: {valor: string, unidad: string, vigenciadesde: string, vigenciahasta: string }[]) => {
-        this.trmPromedio = tasas[0].valor;
-      });
-    setInterval(() => {
-      this.pulseFlag = !this.pulseFlag;
-    }, 600);
+    // this.http.get('https://www.datos.gov.co/api/id/32sa-8pi3.json?$query=select%20*%20order%20by%20`unidad`%20asc%20limit%2040')
+    //   .subscribe((tasas: {valor: string, unidad: string, vigenciadesde: string, vigenciahasta: string }[]) => {
+    //     this.trmPromedio = tasas[0].valor;
+    //   });
+    // setInterval(() => {
+    //   this.pulseFlag = !this.pulseFlag;
+    // }, 600);
   }
 
   // CALCULAR PROMEDIO ÚLTIMOS 20 DÍAS MES VIGENTE (YA NO SE USA)
